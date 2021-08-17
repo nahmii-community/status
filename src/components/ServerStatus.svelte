df2e8c2ad62858961414258366a88ca23bd46fe9
skregas <skregas01@gmail.com>
2021-08-17 16:15:22 +0200

    Add custom styling; refactor status display into separate component

---
+++ src/components/ServerStatus.svelte
@@ -0,0 +1,59 @@
+<script>
+    export let stats;
+    $: console.log(`stats: ${stats}`);
+</script>
+
+{#each stats as {status, name}}
+    <div class="status_line">
+        <p class="service_name">{name}:</p>
+        {#if status}
+            <span class="good_status">Up!</span> 
+        {:else}
+            <span class="bad_status">Down!</span>
+        {/if}
+    </div>
+{/each}
+
+<style>
+.status_line {
+    display: flex; 
+    flex-direction: row
+}
+.service_name {
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 23px;
+    letter-spacing: 0.15px;
+
+    /* Dark Blue */
+    color: #495264;
+    /* color: aliceblue */
+}
+.good_status {
+    width: 100%;
+    color: #00CC39;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 23px;
+    letter-spacing: 0.15px;
+    margin: auto;
+    padding-left: 1rem
+}
+
+.bad_status {
+    width: 100%;
+    color: red;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 23px;
+    letter-spacing: 0.15px;
+    margin: auto;
+    padding-left: 1rem
+}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script>
+	import ServerStatus from '../components/ServerStatus.svelte';
 	import { onMount } from 'svelte';
+
 	let isLoading = true;
 	
 	var myHeaders = new Headers();
@@ -19,10 +21,21 @@
 	redirect: 'follow'
 	};
 	
+	var htmlHeaders = new Headers();
+	htmlHeaders.append("Content-Type", "application/html");
+	
+	var getHtmlRequestOptions = {
+		method: 'GET',
+		headers: htmlHeaders,
+		redirect: 'follow'
+	}
+
 	let servers = [
-			{ id: 'test', name: 'Nahmii Testnet', url: "https://l2-tc3.test.nahmii.net"},
-			{ id: 'ropsten', name: 'Nahmii Ropsten', url: "https://l2.testnet.nahmii.io"},
-			{ id: 'mainnet', name: 'Nahmii Mainnet', url: "https://path/to/mainnet"}
+			// { id: 'test', name: 'Nahmii Testnet', url: "https://l2-tc3.test.nahmii.net"},
+			{ id: 'ropsten', name: 'Ropsten', url: "https://l2.testnet.nahmii.io", options: requestOptions},
+			{ id: 'mainnet', name: 'Mainnet', url: "https://path/to/mainnet", options: requestOptions},
+			{ id: 'explorer', name: 'Explorer', url: "https://explorer.testnet.nahmii.io", options: getHtmlRequestOptions},
+			{ id: 'deployer', name: 'Deployer', url: 'https://meta.testnet.nahmii.io', options: getHtmlRequestOptions}
 		];
 
 	// collection of statuses
@@ -38,12 +51,13 @@
 	async function getServerVersion(server) {
 		let res, json;
 		try {
-			res = await fetch(server.url, requestOptions);
-			json = await res.json();
+			res = await fetch(server.url, server.options);
+			console.log(server.name, res);
+			// json = await res.json();
 		} catch {
 			return {status: false, name: server.name};
 		}
-		if (res.ok) {
+		if (res.ok ) {
 			return {status: true, name: server.name};
 		} else {
 			return {status: false, name: server.name};
@@ -51,37 +65,102 @@
 	}
 	onMount( async() => {
 		await getServers(servers);
+		console.log(statuses);
 		isLoading = false;
 	});
 </script>
-
-<h1 style="font-style: italic">
-	System Status
-</h1>
-
-{#if isLoading}
-	<p>...waiting</p>
-{:else}
-{#each statuses as {status, name}}
-{#if status}
-	<p class="good_status">{name}: Operational! </p>
-{:else}
-	<p class="bad_status">{name}: System down!</p>
-{/if}
-{/each}
-{/if}
-
+<!-- svelte-ignore a11y-missing-attribute -->
+<html class="nahmii-scaling-ethereum-background">
+	<body >
+		<div class="status_card">
+			<div class="status_card_frame">
+				<h1 class="status_title">
+					Nahmii Status
+				</h1>
+				{#if isLoading}
+					<p class="waiting">...waiting</p>
+				{:else}
+					<ServerStatus stats={statuses} />
+				{/if}
+			</div>
+		</div>
+	</body>
+</html>
 
 <style>
-	.good_status {
-		width: 100%;
-		color: green;
-		margin: 5
+	/* status card */
+	.status_card {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		padding: 32px;
+		position: absolute;
+		width: 450px;
+		height: 379px;
+		left: calc(50% - 450px/2);
+		top: 160px;
+		/* White */
+		background: #FFFFFF;
+		/* Shadow */
+		box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.25);
+		border-radius: 12px;
+	}
+	.status_card_frame {
+		position: static;
+		width: 386px;
+		height: 315px;
+		left: 32px;
+		top: 32px;
+
+		/* Inside Auto Layout */
+		flex: none;
+		order: 0;
+		align-self: stretch;
+		flex-grow: 1;
+		margin: 0px 10px;
+		/* color:black;
+		border-color: black;
+		border-width: 5px; */
 	}
 
-	.bad_status {
-		width: 100%;
-		color: red;
-		margin: 5;
+	/* Nahmii Status title */
+	.status_title {
+		background: -webkit-linear-gradient(#FF44C9, #00B8FA);
+		background-clip: unset;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+		/* position: absolute; */
+		width: 167px;
+		height: 20px;
+		left: 0px;
+		top: 0px;
+
+		font-family: Roboto;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 26px;
+		line-height: 20px;
+		/* identical to box height, or 77% */
+
+		letter-spacing: 0.15px;
+	}
+	.waiting {
+		font-family: Roboto;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 26px;
+		line-height: 20px;
+		/* identical to box height, or 77% */
+		color: aliceblue
+	}
+	
+	/* nahmii-scaling-ethereum-background 1 */
+	.nahmii-scaling-ethereum-background {
+		position: absolute;
+		height: 900px;
+		left: 0%;
+		right: 0.04%;
+		top: 0px;
+		background: url("../../static/nahmii-scaling-ethereum-background.png");
 	}
 </style>
