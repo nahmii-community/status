d9f2ab37abbcdccc775b21cd2d8ba67c6069fb92
Hein Dauven <heindauven@gmail.com>
2021-09-24 13:17:11 +0200

    Remove HR, instead use CSS styling for divider.

--- src/components/ServerStatus.svelte
+++ src/components/ServerStatus.svelte
@@ -3,75 +3,67 @@
     $: console.log(`stats: ${stats}`);
 </script>
 
-{#each stats as {status, name}, i}
+{#each stats as { status, name }, i}
     <div class="status_line">
         <p class="service_name">{name}:</p>
         {#if status}
-            <span class="good_status">Up!</span> 
+            <span class="good_status">Up!</span>
         {:else}
             <span class="bad_status">Down!</span>
         {/if}
-        
     </div>
-    {#if i < stats.length - 1}
-        <hr class=status_line_seperator>
-    {/if}
 {/each}
 
 <style>
-.status_line {
-    display: flex; 
-    flex-direction: row
-}
-.service_name {
-    width: 100%;
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 23px;
-    letter-spacing: 0.15px;
+    .status_line {
+        display: flex;
+        flex-direction: row;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+        padding-bottom: 0.75em;
+    }
 
-    /* Dark Blue */
-    color: #495264;
-    /* color: aliceblue */
-}
-.good_status {
-    width: 50%;
-    color: #00CC39;
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 23px;
-    letter-spacing: 0.15px;
-    margin: auto;
-    padding-left: 1rem;
-    text-align: right;
-}
+    .status_line:last-child {
+        border-bottom: none;
+    }
 
-.bad_status {
-    width: 50%;
-    color: red;
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 23px;
-    letter-spacing: 0.15px;
-    margin: auto;
-    padding-left: 1rem;
-    text-align: right;
-}
-.status_line_seperator {
-    
-    height: 0px;
-    left: 0%;
-    right: 0%;
-    top: 107px;
+    .service_name {
+        width: 100%;
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 23px;
+        letter-spacing: 0.15px;
+        /* Dark Blue */
+        color: #495264;
+        /* color: aliceblue */
+    }
 
-/* Light Grey 1 */
+    .good_status {
+        width: 50%;
+        color: #00cc39;
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 23px;
+        letter-spacing: 0.15px;
+        margin: auto;
+        padding-left: 1rem;
+        text-align: right;
+    }
 
-    border: 1px solid rgba(0, 0, 0, 0.2);
-}
+    .bad_status {
+        width: 50%;
+        color: red;
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 23px;
+        letter-spacing: 0.15px;
+        margin: auto;
+        padding-left: 1rem;
+        text-align: right;
+    }
 </style>
