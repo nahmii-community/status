67b2bf13b7f214940aaea75b2df37825cdab577a
Hein Dauven <heindauven@gmail.com>
2021-09-24 10:09:53 +0200

    Remove CSS comments from index route, refactor document.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
-	import ServerStatus from '../components/ServerStatus.svelte';
-	import Header from '../components/Header.svelte';
-	import {onMount} from 'svelte';
+	import ServerStatus from "../components/ServerStatus.svelte";
+	import Header from "../components/Header.svelte";
+	import { onMount } from "svelte";
 
 	let isLoading = true;
 
@@ -9,73 +9,72 @@
 	myHeaders.append("Content-Type", "application/json");
 
 	var raw = JSON.stringify({
-		"jsonrpc": "2.0",
-		"method": "web3_clientVersion",
-		"params": [],
-		"id": 1
+		jsonrpc: "2.0",
+		method: "web3_clientVersion",
+		params: [],
+		id: 1,
 	});
 
 	var requestOptions = {
-		method: 'POST',
+		method: "POST",
 		headers: myHeaders,
 		body: raw,
-		redirect: 'follow'
+		redirect: "follow",
 	};
 
 	var htmlHeaders = new Headers();
 	htmlHeaders.append("Content-Type", "application/html");
 
 	var getHtmlRequestOptions = {
-		method: 'GET',
+		method: "GET",
 		headers: htmlHeaders,
-		redirect: 'follow'
-	}
+		redirect: "follow",
+	};
 
 	let servers = [
 		// { id: 'test', name: 'Nahmii Testnet', url: "https://l2-tc3.test.nahmii.net"},
 		{
-			id: 'mainnet-node',
-			name: 'Mainnet Node',
+			id: "mainnet-node",
+			name: "Mainnet Node",
 			url: "https://path/to/mainnet/node",
-			options: requestOptions
+			options: requestOptions,
 		},
 		{
-			id: 'mainnet-explorer',
-			name: 'Mainnet Explorer',
+			id: "mainnet-explorer",
+			name: "Mainnet Explorer",
 			url: "https://path/to/mainnet/explorer",
-			options: requestOptions
+			options: requestOptions,
 		},
 		{
-			id: 'mainnet-meta-service',
-			name: 'Mainnet Meta Service',
+			id: "mainnet-meta-service",
+			name: "Mainnet Meta Service",
 			url: "https://path/to/mainnet/meta/service",
-			options: requestOptions
+			options: requestOptions,
 		},
 		{
-			id: 'testnet-node',
-			name: 'Testnet Node',
+			id: "testnet-node",
+			name: "Testnet Node",
 			url: "https://l2.testnet.nahmii.io",
-			options: requestOptions
+			options: requestOptions,
 		},
 		{
-			id: 'testnet-explorer',
-			name: 'Testnet Explorer',
+			id: "testnet-explorer",
+			name: "Testnet Explorer",
 			url: "https://explorer.testnet.nahmii.io",
-			options: getHtmlRequestOptions
+			options: getHtmlRequestOptions,
 		},
 		{
-			id: 'testnet-meta-service',
-			name: 'Testnet Meta Service',
-			url: 'https://meta.testnet.nahmii.io',
-			options: getHtmlRequestOptions
-		}
+			id: "testnet-meta-service",
+			name: "Testnet Meta Service",
+			url: "https://meta.testnet.nahmii.io",
+			options: getHtmlRequestOptions,
+		},
 	];
 
 	// collection of statuses
 	export let statuses = [];
 
 	async function getServers(servers) {
-
 		for (let server of servers) {
 			statuses = [...statuses, await getServerVersion(server)];
 		}
@@ -88,12 +87,12 @@
 			console.log(server.name, res);
 			// json = await res.json();
 		} catch {
-			return {status: false, name: server.name};
+			return { status: false, name: server.name };
 		}
 		if (res.ok) {
-			return {status: true, name: server.name};
+			return { status: true, name: server.name };
 		} else {
-			return {status: false, name: server.name};
+			return { status: false, name: server.name };
 		}
 	}
 
@@ -103,27 +102,25 @@
 		isLoading = false;
 	});
 </script>
+
 <!-- svelte-ignore a11y-missing-attribute -->
 <html class="nahmii-scaling-ethereum-background">
-<body>
-<Header/>
-<div class="status_card">
-	<div class="status_card_frame">
-		<h1 class="status_title">
-			Nahmii Status
-		</h1>
-		{#if isLoading}
-			<p class="waiting">...waiting</p>
-		{:else}
-			<ServerStatus stats={statuses}/>
-		{/if}
-	</div>
-</div>
-</body>
+	<body>
+		<Header />
+		<div class="status_card">
+			<div class="status_card_frame">
+				<h1 class="status_title">Nahmii Status</h1>
+				{#if isLoading}
+					<p class="waiting">...waiting</p>
+				{:else}
+					<ServerStatus stats={statuses} />
+				{/if}
+			</div>
+		</div>
+	</body>
 </html>
 
 <style>
-	/* status card */
 	.status_card {
 		display: flex;
 		flex-direction: column;
@@ -134,9 +131,7 @@
 		height: 530px;
 		left: calc(50% - 450px / 2);
 		top: 160px;
-		/* White */
-		background: #FFFFFF;
-		/* Shadow */
+		background: #ffffff;
 		box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.25);
 		border-radius: 12px;
 	}
@@ -144,20 +139,15 @@
 	.status_card_frame {
 		left: 32px;
 		top: 32px;
-		/* Inside Auto Layout */
 		flex: none;
 		order: 0;
 		align-self: stretch;
 		flex-grow: 1;
 		margin: 0px 10px;
-		/* color:black;
-        border-color: black;
-        border-width: 5px; */
 	}
 
-	/* Nahmii Status title */
 	.status_title {
-		background: -webkit-linear-gradient(45deg,#FF44C9 0%, #00B8FA 100%);
+		background: -webkit-linear-gradient(45deg, #ff44c9 0%, #00b8fa 100%);
 		background-clip: unset;
 		-webkit-background-clip: text;
 		-webkit-text-fill-color: transparent;
@@ -172,8 +162,6 @@
 		font-weight: bold;
 		font-size: 26px;
 		line-height: 20px;
-		/* identical to box height, or 77% */
-
 		letter-spacing: 0.15px;
 	}
 
@@ -183,11 +171,9 @@
 		font-weight: bold;
 		font-size: 26px;
 		line-height: 20px;
-		/* identical to box height, or 77% */
-		color: aliceblue
+		color: aliceblue;
 	}
 
-	/* nahmii-scaling-ethereum-background 1 */
 	.nahmii-scaling-ethereum-background {
 		position: absolute;
 		height: 100vh;
