eefcad786fdbe2b45388ed7ebf21ac2355c1e7e8
Hein Dauven <heindauven@gmail.com>
2021-09-25 17:24:21 +0200

    Add styling for description line. Display description if service down.

--- src/components/ServerStatus.svelte
+++ src/components/ServerStatus.svelte
@@ -1,29 +1,42 @@
 <script>
     export let statuses;
-    $: console.log(`statuses: ${statuses}`);
 </script>
 
-{#each statuses as { status, name }, i}
-    <div class="status-line">
-        <p class="service-name">{name}:</p>
-        {#if status}
-            <span class="good-status">Up!</span>
-        {:else}
-            <span class="bad-status">Down!</span>
-        {/if}
+{#each statuses as { status, name, description }, i}
+    <div class="status">
+        <div class="status-line">
+            <p class="service-name">{name}:</p>
+            {#if status}
+                <span class="good-status">Up!</span>
+            {:else}
+                <span class="bad-status">Down!</span>
+            {/if}
+        </div>
+        <p class="status-description">{description ? description : ""}</p>
     </div>
 {/each}
 
 <style>
+    .status {
+        display: flex;
+        flex-direction: column;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+        padding-bottom: 0.5em;
+        margin-bottom: 0.5em;
+    }
+
+    .status:last-child {
+        border-bottom: none;
+        margin-bottom: 0;
+    }
+
     .status-line {
         display: flex;
         flex-direction: row;
-        border-bottom: 1px solid rgba(0, 0, 0, 0.2);
-        padding-bottom: 0.75em;
     }
 
-    .status-line:last-child {
-        border-bottom: none;
+    .status-line p {
+        margin: 0.4em 0;
     }
 
     .service-name {
@@ -39,7 +52,19 @@
         /* color: aliceblue */
     }
 
-    .good-status, .bad-status {
+    .status-description {
+        margin: 0;
+        color: var(--text-color-medium-grey);
+        font-size: 14px;
+        min-height: 16px;
+    }
+
+    .status:last-child .status-description {
+        min-height: 0;
+    }
+
+    .good-status,
+    .bad-status {
         width: 50%;
         font-family: Roboto;
         font-style: normal;
